f5275797f15c5fc8c47e414023a98fe4ff4e2254
fatihul04 <fatihul978@gmail.com>
2025-06-12 11:55:53 +0800

    masukkan 5 data ke dalam array

--- hallo.cpp
+++ hallo.cpp
@@ -2,4 +2,8 @@
 #include <string.h>
 int main() {
     char nama[5][50]; 
-    
+    strcpy(nama[0], "Fadli");
+    strcpy(nama[1], "Ijan");
+    strcpy(nama[2], "Pepen");
+    strcpy(nama[3], "Rayhan");
+    strcpy(nama[4], "Abel");
